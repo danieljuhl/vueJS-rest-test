b7c4b4ef82099f68cd564521109005e1dd7d1cff
lauridsen <pjuker@gmail.com>
2018-04-04 17:46:00 +0200

    Added loading indicator when fetching in Single component

--- src/components/Single.vue
+++ src/components/Single.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="modal" :style="{display: isDisplayed}">
       <div class="modal-content">
-        <div class="card-content">
-          <p class="title">{{ coin.display_name }}</p>
-          <p class="subtitle">{{ coin.id }}</p>
+        <div v-if="!isLoading">
+          <div class="card-content">
+            <p class="title">{{ coin.display_name }}</p>
+            <p class="subtitle">{{ coin.id }}</p>
+          </div>
+          <p class="subtitle">
+            Market Cap: {{coin.market_cap | toCurrency}}
+          </p>
+          <p class="subtitle">
+            Supply: {{coin.supply | toCurrency}}
+          </p>
+          <p class="subtitle">
+            24 Hour Volume: {{coin.volume | toCurrency}}
+          </p>
+          <button class="button is-primary" @click="$router.go(-1)">Close</button>
         </div>
-        <p class="subtitle">
-          Market Cap: {{coin.market_cap | toCurrency}}
-        </p>
-        <p class="subtitle">
-          Supply: {{coin.supply | toCurrency}}
-        </p>
-        <p class="subtitle">
-          24 Hour Volume: {{coin.volume | toCurrency}}
-        </p>
-        <button class="button is-primary" @click="$router.go(-1)">Close</button>
+        <div v-else-if="isLoading">
+          <div class="card-content">
+            <object data="/static/spinner.svg" type="image/svg+xml">
+              <img src="/static/spinner.gif">
+            </object>
+          </div>
+        </div>
       </div>
     </div>
 </template>
@@ -28,15 +37,18 @@ export default {
         return {
             coin: {},
             isDisplayed: 'none',
+            isLoading: true,
         };
     },
     created: function() {
         axios
             .get('http://coincap.io/page/' + this.$route.params.id)
             .then(res => {
+                this.isLoading = false;
                 this.coin = res.data;
             })
             .catch(err => {
+                this.isLoading = false;
                 console.log(err);
             });
         this.isDisplayed = 'block';
