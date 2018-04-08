c3fe54896bced461ca62e3c501ee86ca432fbcc2
lauridsen <pjuker@gmail.com>
2018-04-08 21:36:06 +0200

    changed directory pathing

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ var deferredPrompt;
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-        .register('/sw.js')
+        .register('sw.js')
         .then(function() {
             console.log('Service Worker Registered');
         })
